f57f94ab2ad591ade8636f4f61e568ea2ebaffa5
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-11-30 01:44:03 -0330

    Try new stuff

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,17 +20,21 @@ dnf5 install -y tmux
 
 ## MULTIMEDIA PACKAGES
 dnf5 swap -y ffmpeg-free ffmpeg --allowerasing
-dnf5 update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+# dnf5 update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
 ## DESKTOP ENVIRONMENT
 # Install XFCE Desktop Environment and a lightweight Display Manager
 dnf5 install -y @xfce-desktop lightdm-gtk
+# Install XFCE utilities
+dnf5 install -y blueman
+
+## PACKAGE MANAGEMENT
+dnf5 install -y gnome-software
 
 ## CHROMEBOOK PACKAGES
 # Install Chromebook-specific keyboard configuration for hardware compatibility
 # Note: This package handles function keys, trackpad, and media buttons.
-dnf5 remove -y --noautoremove xkeyboard-config # Conflicts with the gallium os xkeyboard config
-dnf5 install -y https://github.com/sihawken/xkeyboard-config-galliumos-rpm/releases/download/5eb120c/xkeyboard-config-galliumos-1.0.0-1.fc42.x86_64.rpm
+dnf5 swap -y --allowerasing xkeyboard-config https://github.com/sihawken/xkeyboard-config-galliumos-rpm/releases/download/5eb120c/xkeyboard-config-galliumos-1.0.0-1.fc42.x86_64.rpm
 
 # Install chromebook linux audio
 dnf5 install -y chromebook-linux-audio
