5ad113579403bde939e04601dca2c31efee019ba
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-01 06:13:20 -0330

    clean build file

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,19 +14,13 @@ set -ouex pipefail
 dnf5 -y copr enable sihawken/xkeyboard-config-galliumos-rpm
 # Repository for TLPUI
 dnf5 -y copr enable sunwire/tlpui fedora-$(rpm -E %fedora)-x86_64
-# # RPMfusion repos
+# RPMfusion repos
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-## CHROMEBOOK PACKAGES
-# Enables keyboard layouts for chromebooks
-# dnf5 -y swap xkeyboard-config xkeyboard-config-galliumos-rpm
 
 ## CHROMEBOOK AUDIO (Install UCM configuration)
 git clone --depth 1 https://github.com/WeirdTreeThing/alsa-ucm-conf-cros -b standalone /tmp/alsa-ucm-conf-cros
 cp -a /tmp/alsa-ucm-conf-cros/ucm2 /usr/share/alsa/
 cp -a /tmp/alsa-ucm-conf-cros/overrides /usr/share/alsa/ucm2/conf.d/
-
-# rpm-ostree override remove xkeyboard-config --install xkeyboard-config-galliumos-rpm
 
 ## CINNAMON DESKTOP
 dnf5 -y install @cinnamon-desktop
@@ -36,16 +30,15 @@ dnf5 -y remove firefox pidgin xawtv thunderbird hexchat xfburn shotwell transmis
 dnf -y install lightdm slick-greeter lightdm-settings
 echo -e "[Seat:*]\ngreeter-session=slick-greeter" | tee /etc/lightdm/lightdm.conf.d/99-slick-greeter.conf
 
-# ## MULTIMEDIA PACKAGES
+## MULTIMEDIA PACKAGES
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y install intel-media-driver libva-intel-driver
 dnf5 -y swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing
 dnf5 -y install mesa-vdpau-drivers-freeworld
 
-# Install power and performance optimization tools
+## PERFORMANCE OPTIMIZATION
 dnf5 install -y zram-generator
 
-## OPTIMIZATION
 # Zram optimization
 tee /etc/systemd/zram-generator.conf > /dev/null <<EOF
 [zram0]
