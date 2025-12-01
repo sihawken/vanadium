3ddf395070af37a3719de1d8f399503b0c344fc0
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-01 05:35:05 -0330

    zram optimization

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,6 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 ## REPOS
-# Repository to enable audio support on chromebook devices
-dnf5 -y copr enable pvermeer/chromebook-linux-audio fedora-$(rpm -E %fedora)-x86_64
 # Repository that adds the gallium os xkeyboard-config 
 dnf5 -y copr enable sihawken/xkeyboard-config-galliumos-rpm
 # Repository for TLPUI
@@ -22,8 +20,6 @@ dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 ## CHROMEBOOK PACKAGES
 # Enables keyboard layouts for chromebooks
 # dnf5 -y swap xkeyboard-config xkeyboard-config-galliumos-rpm
-# # Fixes audio issues for chromebooks
-# dnf5 install -y chromebook-linux-audio
 
 ## CHROMEBOOK AUDIO (Install UCM configuration)
 git clone --depth 1 https://github.com/WeirdTreeThing/alsa-ucm-conf-cros -b standalone /tmp/alsa-ucm-conf-cros
@@ -46,19 +42,19 @@ dnf5 -y install intel-media-driver libva-intel-driver
 dnf5 -y swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing
 dnf5 -y install mesa-vdpau-drivers-freeworld
 
-# 3. Install power and performance optimization tools
-# dnf5 install -y tlp tlp-rdw tlpui zram-generator
+# Install power and performance optimization tools
+dnf5 install -y tlp tlp-rdw tlpui zram-generator
 
-# ## OPTIMIZATION
-# # Zram optimization
-# tee /etc/systemd/zram-generator.conf > /dev/null <<EOF
-# [zram0]
-# zram-size = ram * 1.5
-# compression-algorithm = lz4
-# swap-priority = -1
-# EOF
+## OPTIMIZATION
+# Zram optimization
+tee /etc/systemd/zram-generator.conf > /dev/null <<EOF
+[zram0]
+zram-size = ram * 1.5
+compression-algorithm = lz4
+swap-priority = -1
+EOF
 
-# Create a file to limit streams to 2
+# # Create a file to limit streams to 2
 # mkdir -p /etc/systemd/system/systemd-zram-setup@zram0.service.d/
 # tee /etc/systemd/system/systemd-zram-setup@zram0.service.d/override.conf > /dev/null <<EOF
 # [Service]
