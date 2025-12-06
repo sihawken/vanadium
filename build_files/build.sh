f94b50d1661c1258d4d2344f821ac797e04f4d42
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-06 16:17:25 -0330

    change command

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-releasever=$(/usr/bin/lsb_release -rs)
+releasever=$(/usr/bin/rpm -E %fedora)
 basearch=$(/usr/bin/arch)
 
 ## DNF5 Speedup
