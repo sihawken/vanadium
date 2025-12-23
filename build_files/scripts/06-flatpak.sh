c940059aee40e932cf32cda157dfa81da1101a6f
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-23 13:35:15 -0800

    remove unknown

--- build_files/scripts/06-flatpak.sh
+++ build_files/scripts/06-flatpak.sh
@@ -15,7 +15,4 @@ curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub
 # Add cosmic repo
 curl --retry 3 -Lo /etc/flatpak/remotes.d/cosmic.flatpakrepo https://apt.pop-os.org/cosmic/cosmic.flatpakrepo
 
-# Fedora Flatpak service is a part of the flatpak package, ensure it's overridden by moving to replace it at the end of the build.
-mv -f /usr/lib/systemd/system/flatpak-add-flathub-repos.service /usr/lib/systemd/system/flatpak-add-fedora-repos.service
-
 
