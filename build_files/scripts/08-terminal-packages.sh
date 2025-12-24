fbad353364f607d2cfaae805f148099c1291793b
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-23 23:05:55 -0800

    add key bindings

--- build_files/scripts/08-terminal-packages.sh
+++ build_files/scripts/08-terminal-packages.sh
@@ -23,6 +23,10 @@ source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
 
+# Keybindings for History Substring Search (Up/Down arrows)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
 # History
 HISTSIZE=10000
 SAVEHIST=10000
