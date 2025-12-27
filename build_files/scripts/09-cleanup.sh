ff6e6d3ae4ce50560eef5d8e670103533c976f7c
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-26 23:45:21 -0800

    Enable rpm-ostree timer

--- build_files/scripts/09-cleanup.sh
+++ build_files/scripts/09-cleanup.sh
@@ -2,6 +2,12 @@
 
 set -eoux pipefail
 
+# Enable Update Timers
+systemctl enable rpm-ostreed-automatic.timer
+
+# Configure staged updates for rpm-ostree
+cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
+
 dnf5 clean all
 rm -rf /tmp/* || true
 rm -rf /var/log/dnf5.log || true
